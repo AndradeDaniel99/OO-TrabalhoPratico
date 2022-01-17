b0da099e7ee0c84d187710d0f25496d56dc4bfcd
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-01-17 17:01:26 -0300

    estrutura do menu vazio implementada.
    os comandos do menu estao funcionando, porem nao chamam nenhum metodo
    ainda.

--- tp1/src/tp1/Tp1.java
+++ tp1/src/tp1/Tp1.java
@@ -1,11 +1,51 @@
+/*Escreva um programa em Java que implemente o jogo da forca. 
+O seu jogo deve exibir um menu inicial com as opções: 
+“1. Gerenciar Temas”, “2. Gerenciar Palavras”, “3. Jogar” e “4. Sair”.*/
+
 package tp1;
+
+import java.util.Scanner;
 
 public class Tp1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
-
+		int menu;
+		Scanner ler = new Scanner(System.in);
+		
+		do {
+			System.out.println("1. Gerenciar Temas");
+			System.out.println("2. Gerenciar Palavras");
+			System.out.println("3. Jogar");
+			System.out.println("4. Sair");
+			menu = ler.nextInt();
+			
+			switch(menu) {
+				case 1:
+					clearScreen();
+					//gerenciarTemas();
+					break;
+				case 2:
+					clearScreen();
+					//gerenciarPalavras();
+					break;
+				case 3:
+					clearScreen();
+					//jogar();
+					break;
+				default:
+					clearScreen();
+					break;
+			}
+			
+		}while(menu>0 && menu<4);
+		
 	}
 
+	public static void clearScreen() {
+		for(int i=0; i<30; i++) {
+			System.out.print("\n");
+		}
+	}
+	
 }
