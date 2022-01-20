7475ffceb11130d08470a942e8facb27b580cbe2
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-01-20 20:50:38 -0300

    comecei a implementar a funcao gerenciarTemas() e o cadastro
    de temas ja esta funcionando

--- tp1/src/tp1/Tp1.java
+++ tp1/src/tp1/Tp1.java
@@ -7,23 +7,26 @@ package tp1;
 import java.util.Scanner;
 
 public class Tp1 {
-
+	
+	static String temasEPalavras[][] = new String [50][51];
+	
 	public static void main(String[] args) {
 		
-		int menu;
 		Scanner ler = new Scanner(System.in);
+		
+		int menu;
 		
 		do {
 			System.out.println("1. Gerenciar Temas");
 			System.out.println("2. Gerenciar Palavras");
 			System.out.println("3. Jogar");
 			System.out.println("4. Sair");
+			
 			menu = ler.nextInt();
 			
 			switch(menu) {
 				case 1:
-					clearScreen();
-					//gerenciarTemas();
+					gerenciarTemas();
 					break;
 				case 2:
 					clearScreen();
@@ -37,15 +40,71 @@ public class Tp1 {
 					clearScreen();
 					break;
 			}
-			
 		}while(menu>0 && menu<4);
-		
 	}
 
 	public static void clearScreen() {
 		for(int i=0; i<30; i++) {
 			System.out.print("\n");
 		}
+	}
+	
+	public static void gerenciarTemas() {
+		
+		int menu;
+		
+		clearScreen();
+		do {
+			System.out.println("1. Cadastrar Tema");
+			System.out.println("2. Excluir Tema");
+			System.out.println("3. Buscar Tema");
+			System.out.println("4. Voltar");
+			
+			Scanner ler = new Scanner(System.in);
+			menu = ler.nextInt();
+			
+			switch(menu) {
+				case 1:
+					clearScreen();
+					System.out.println("Digite o Tema a ser cadastrado:");
+					String tema = ler.next();
+					
+					for(int i=0; i<50; i++) {
+						String comparador=temasEPalavras[i][0];
+						int confere_tema=1;
+						
+						if(temasEPalavras[i][0]==null){
+							temasEPalavras[i][0]=tema;
+							System.out.println("Tema cadastrado com sucesso!");
+							break;
+						}  
+						
+						confere_tema = tema.compareTo(comparador);
+						if(confere_tema==0) {
+							System.out.println("Impossivel. Esse tema ja existe.");
+							break;
+						}
+					}
+					break;
+				case 2:
+					clearScreen();
+					//gerenciarPalavras();
+					break;
+				case 3:
+					clearScreen();
+					System.out.println("Buscar tema:");
+					//String busca = ler.next();
+					for(int i=0; i<50; i++) {
+						System.out.println(temasEPalavras[i][0]);
+					}
+					break;
+				default:
+					clearScreen();
+					break;
+			}
+		}while(menu>0 && menu<4);
+		
+		
 	}
 	
 }
