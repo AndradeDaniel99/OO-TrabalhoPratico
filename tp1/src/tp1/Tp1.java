b6390badda0a6a5ef2bce96027df10d3dfa63883
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-01-21 09:59:04 -0300

    implementado funcao de excluir temas

--- tp1/src/tp1/Tp1.java
+++ tp1/src/tp1/Tp1.java
@@ -36,9 +36,13 @@ public class Tp1 {
 					clearScreen();
 					//jogar();
 					break;
-				default:
+				case 4:
 					clearScreen();
 					break;
+				default:
+					while(menu>4 || menu<0) {
+						menu = ler.nextInt();
+					}
 			}
 		}while(menu>0 && menu<4);
 	}
@@ -50,10 +54,10 @@ public class Tp1 {
 	}
 	
 	public static void gerenciarTemas() {
+		clearScreen();
 		
 		int menu;
 		
-		clearScreen();
 		do {
 			System.out.println("1. Cadastrar Tema");
 			System.out.println("2. Excluir Tema");
@@ -64,32 +68,62 @@ public class Tp1 {
 			menu = ler.nextInt();
 			
 			switch(menu) {
+			
 				case 1:
 					clearScreen();
 					System.out.println("Digite o Tema a ser cadastrado:");
 					String tema = ler.next();
 					
 					for(int i=0; i<50; i++) {
-						String comparador=temasEPalavras[i][0];
-						int confere_tema=1;
-						
 						if(temasEPalavras[i][0]==null){
 							temasEPalavras[i][0]=tema;
+							clearScreen();
 							System.out.println("Tema cadastrado com sucesso!");
 							break;
-						}  
-						
+						}
+						String comparador=temasEPalavras[i][0];
+						int confere_tema=1;
 						confere_tema = tema.compareTo(comparador);
 						if(confere_tema==0) {
+							clearScreen();
 							System.out.println("Impossivel. Esse tema ja existe.");
 							break;
 						}
 					}
 					break;
+					
 				case 2:
-					clearScreen();
-					//gerenciarPalavras();
+					boolean excluir_count=false;
+					do {
+						clearScreen();
+						System.out.println("Digite o Tema a ser excluido:");
+						String excluir = ler.next();
+						
+						for(int i=0; i<50; i++) {
+							String comparador=temasEPalavras[i][0];
+							int confere_tema=1;
+							confere_tema = excluir.compareTo(comparador);
+						
+							if(confere_tema==0){
+								for(int j=1; j<51; j++) {
+									if(temasEPalavras[i][j]!=null) {
+										System.out.println("Não foi possível excluir o tema. Verifique se existem palavras cadastradas nesse tema.");
+										break;
+									}
+								}
+								temasEPalavras[i][0]=null;
+								excluir_count=true;
+								clearScreen();
+								System.out.println("Tema excluido com sucesso!");
+								break;
+							}
+						}
+						if(!excluir_count) {
+							System.out.println("Tema nao encontrado. Digite novamente:");
+						}
+					}while(excluir_count==false);
 					break;
+					
 				case 3:
 					clearScreen();
 					System.out.println("Buscar tema:");
@@ -98,9 +132,13 @@ public class Tp1 {
 						System.out.println(temasEPalavras[i][0]);
 					}
 					break;
-				default:
+				case 4:
 					clearScreen();
 					break;
+				default:
+					while(menu>4 || menu<0) {
+						menu = ler.nextInt();
+					}
 			}
 		}while(menu>0 && menu<4);
 		
