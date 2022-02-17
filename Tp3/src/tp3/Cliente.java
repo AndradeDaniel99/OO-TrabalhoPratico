138d094d2d326ded6fab152ababe7a50d960242e
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-02-16 21:40:31 -0300

    classe pai contato implementada

---
+++ Tp3/src/tp3/Cliente.java
@@ -0,0 +1,5 @@
+package tp3;
+
+public final class Cliente extends Contato {
+
+}
--- Tp3/src/tp3/Contato.java
+++ Tp3/src/tp3/Contato.java
@@ -1,5 +1,52 @@
 package tp3;
 
 public abstract class Contato {
-
+	
+	
+	protected String nome;
+	protected String endereco;
+	protected String email;
+	protected String biografia;
+	protected int cpf;
+	protected int telefone;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBiografia() {
+		return biografia;
+	}
+	public void setBiografia(String biografia) {
+		this.biografia = biografia;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	
+	
 }
